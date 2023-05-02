dbf47f6171abbc16c56386e79e9b1e1ff42b4915
gambit101 <mohammadsamraj@gmail.com>
2023-05-02 12:59:07 +0600

    added login and register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>this is login</h2>
+        <div className='mt-5'>
+            <Form className='w-50 ms-5'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className='text-warning'>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label className='text-warning'>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check className='text-warning' type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="warning" type="submit">
+                    Submit
+                </Button>
+                <br />
+                <Button className='mt-3' variant="warning" type="submit"><FaGoogle></FaGoogle> Login With Google
+                </Button>
+                <br />
+                <Button className='mt-3' variant="warning" type="submit"><FaGithub></FaGithub> Login With Github
+                </Button>
+
+                <p className='text-warning mt-3'>Don't have an account? <Link to='/register'>Register</Link> </p>
+
+            </Form>
         </div>
     );
 };
