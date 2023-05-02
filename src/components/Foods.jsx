90515065fec40d772146aaa022cf17c89c16abc4
gambit101 <mohammadsamraj@gmail.com>
2023-05-03 03:14:57 +0600

    added toaster

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { useLoaderData, useParams} from 'react-router-dom';
-
+import React, { useState } from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
+import { Button, Toast } from 'react-bootstrap';
 
 const Foods = () => {
+    const [showToast, setShowToast] = useState(false);
+    const [secondToast, setSecondToast] = useState(false);
+    const [thirdToast, setThirdToast] = useState(false);
+    const [disableButton, setDisableButton] = useState(false);
+    const [disableButton2, setDisableButton2] = useState(false);
+    const [disableButton3, setDisableButton3] = useState(false);
 
     // const {id} = useParams();
 
@@ -10,14 +16,14 @@ const Foods = () => {
     console.log(foods);
 
     // const {experience} = foods;
-    
+
     // console.log(id);
 
     return (
         <div>
             <div className='d-flex container gap-3 mt-5'>
                 <div>
-                    <img style={{width: '500px'}} className='rounded' src={foods.chef_picture} alt="" />
+                    <img style={{ width: '500px' }} className='rounded' src={foods.chef_picture} alt="" />
                 </div>
                 <div>
                     <h2 className='text-warning fw-bold'>{foods.chef_name}</h2>
@@ -31,11 +37,11 @@ const Foods = () => {
             {/* recipe */}
 
             <h1 className='text-center text-warning mt-5'>{foods.chef_name} recipes</h1>
-            <hr className='text-white'/>
+            <hr className='text-white' />
 
             <div className='d-flex container gap-3 mt-5 h-25'>
-                <div  style={{height: '400px'}}>
-                    <img className=' h-75 rounded' style={{width: '400px'}} src={foods.r1_img} alt="" />
+                <div style={{ height: '400px' }}>
+                    <img className=' h-75 rounded' style={{ width: '400px' }} src={foods.r1_img} alt="" />
                 </div>
 
                 <div>
@@ -43,18 +49,32 @@ const Foods = () => {
                         {foods.recipe1Name}
                     </h2>
                     <h4 className='text-white'>
-                    <span className='text-warning'> Ingredients:</span> {foods.ingredients}
+                        <span className='text-warning'> Ingredients:</span> {foods.ingredients}
                     </h4>
                     <p className='text-white'>
                         Recipe: {foods.recipe1}
                     </p>
-                    <button type="button" class="ms-auto btn btn-warning fw-bold">Favorite</button>
+                    <Button onClick={() => {
+                        setShowToast(true);
+                        setDisableButton(true);
+                    }} disabled={disableButton}>Favorite</Button>
+                    <div>
+                        <Toast className='w-50 ms-auto'  show={showToast} onClose={() => {
+                            setShowToast(false);
+                            
+                        }}>
+                            <Toast.Header>
+                                <strong className="ms-auto">{foods.recipe1Name}</strong>
+                            </Toast.Header>
+                            <Toast.Body className='text-primary fw-bold'>Added to favorite</Toast.Body>
+                        </Toast>
+                    </div>
                 </div>
 
             </div>
             <div className='d-flex container gap-3 mt-4 '>
                 <div>
-                    <img className=' h-75 rounded' style={{width: '400px'}} src={foods.r2_img} alt="" />
+                    <img className=' h-75 rounded' style={{ width: '400px' }} src={foods.r2_img} alt="" />
                 </div>
 
                 <div>
@@ -67,13 +87,27 @@ const Foods = () => {
                     <p className='text-white'>
                         Recipe: {foods.recipe2}
                     </p>
-                    <button type="button" class="ms-auto btn btn-warning fw-bold">Favorite</button>
+                    <Button onClick={() => {
+                        setSecondToast(true);
+                        setDisableButton2(true);
+                    }} disabled={disableButton2}>Favorite</Button>
+                    <div>
+                        <Toast className='w-50 ms-auto'  show={secondToast} onClose={() => {
+                            setSecondToast(false);
+                            
+                        }}>
+                            <Toast.Header>
+                                <strong className="ms-auto">{foods.recipe2Name}</strong>
+                            </Toast.Header>
+                            <Toast.Body className='text-primary fw-bold'>Added to favorite</Toast.Body>
+                        </Toast>
+                    </div>
                 </div>
 
             </div>
             <div className='d-flex container gap-3 mt-4 h-25'>
                 <div>
-                    <img className=' h-75 rounded' style={{width: '400px'}} src={foods.r3_img} alt="" />
+                    <img className=' h-75 rounded' style={{ width: '400px' }} src={foods.r3_img} alt="" />
                 </div>
 
                 <div>
@@ -81,16 +115,30 @@ const Foods = () => {
                         {foods.recipe3Name}
                     </h2>
                     <h4 className='text-white'>
-                    <span className='text-warning'> Ingredients:</span> {foods.ingredients}
+                        <span className='text-warning'> Ingredients:</span> {foods.ingredients}
                     </h4>
                     <p className='text-white'>
                         Recipe: {foods.recipe3}
                     </p>
-                    <button type="button" class="ms-auto btn btn-warning fw-bold">Favorite</button>
+                    <Button onClick={() => {
+                        setThirdToast(true);
+                        setDisableButton3(true);
+                    }} disabled={disableButton3}>Favorite</Button>
+                    <div>
+                        <Toast className='w-50 ms-auto'  show={thirdToast} onClose={() => {
+                            setThirdToast(false);
+                            
+                        }}>
+                            <Toast.Header>
+                                <strong className="ms-auto">{foods.recipe3Name}</strong>
+                            </Toast.Header>
+                            <Toast.Body className='text-primary fw-bold'>Added to favorite</Toast.Body>
+                        </Toast>
+                    </div>
                 </div>
 
             </div>
-            
+
         </div>
     );
 };
